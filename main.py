4fbb662311829cc840d848601806c44eeee98c7d
Arthur Chang <arthurc.iphone@gmail.com>
2016-10-20 02:23:27 +0800

    add connection check

--- main.py
+++ main.py
@@ -4,69 +4,90 @@ import config  # setting for this project
 from contextlib import closing
 
 
-# list for the student that have already vote
-stu_id_list = []
+# globel
+stu_id_list = []  # list for the student that have already vote
+
 
 # load the vote history
-try:
-    f = open('./list.txt', 'r')
-    for line in f:
-        stu_id_list.append(line)
-except Exception as e:
-    print('read file error')
-    f.close()
-    exit()
-else:
-    print('load file done\n')
+def load_file():
+    try:
+        f = open('./list.txt', 'r')
+        for line in f:
+            stu_id_list.append(line)
+    except Exception as e:
+        print('read file error')
+        f.close()
+        return False
+    else:
+        print('load file done\n')
+        return True
+
+
+# parse the return hex value and print the card id out
+# if the id haven't vote before write it to file
+def card_id_process(return_value_hex):
+    # initial
+    card_id = ''
+
+    # change card id from hex to dex
+    return_value_dec = [int(hex_string, 0) for hex_string in return_value_hex]
+
+    # format card id (length have to be 5)
+    card_id_site = str(return_value_dec[19] * 256 + return_value_dec[20])
+    card_id_card = str(return_value_dec[23] * 256 + return_value_dec[24])
+    for card_id_part in [card_id_site, card_id_card]:
+        not_enough_len = 5 - len(card_id_site)
+        if not not_enough_len:
+            for index in range(0, not_enough_len):
+                card_id_part = '0{0}'.format(card_id_part)
+
+        card_id += card_id_part
+
+    # if the card id have not vote write into list.txt
+    if card_id not in stu_id_list:
+        stu_id_list.append(card_id)
+        with open('./list.txt', 'a') as stu_id_list_file:
+            stu_id_list_file.write('{0}\n'.format(card_id))
+
+        print('{0} have not vote before'.format(card_id))
+    else:
+        print('{0} have already vote before'.format(card_id))
+
 
 # build socket
-with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as connection:
-    # Connect to the card reader
-    connection.connect(config.ADDRESS)
+def connect_and_read_input():
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as connection:
+        # Connect to the card reader
+        try:
+            connection.connect(config.ADDRESS)
+        except Exception as e:
+            print('can\'t connect to card reader, please check it')
+            exit()
 
-    # Clear all card reader history
-    cmd = config.CMD_CLEAR_ALL
-    connection.send(bytes(cmd))
+        # Clear all card reader history
+        connection.send(bytes(config.CMD_CLEAR_ALL))
 
-    while True:
-        # read the list record
-        cmd = config.CMD_READ_LAST
-        connection.send(bytes(cmd))
+        while True:
+            # read the list record
+            connection.send(bytes(config.CMD_READ_LAST))
 
-        # get reading list record result
-        return_value_byte = connection.recv(config.BUFFER_SIZE)
-        return_value_hex = [hex(byte) for byte in return_value_byte]
+            # get reading list record result
+            return_value_byte = connection.recv(config.BUFFER_SIZE)
+            return_value_hex = [hex(byte) for byte in return_value_byte]
 
-        # check if that is a card id or not
-        if return_value_hex[1] == '0x1d':
-            # change card id from hex to dex
-            return_value_dec = [int(hex_string, 0) for hex_string in return_value_hex]
+            # check if that is a card id or not
+            if return_value_hex[1] == '0x1d':
+                card_id_process(return_value_hex)
 
-            # format card id (length have to be 5)
-            card_id_site = str(return_value_dec[19] * 256 + return_value_dec[20])
-            card_id_card = str(return_value_dec[23] * 256 + return_value_dec[24])
-            for card_id_part in [card_id_site, card_id_card]:
-                not_enough_len = 5 - len(card_id_site)
-                if not not_enough_len:
-                    for index in range(0, not_enough_len):
-                        card_id_part = '0{0}'.format(card_id_part)
+                # clear the id from card reader that get this time
+                connection.send(bytes(config.CMD_CLEAR_LAST))
 
-                card_id += card_id_part
 
-            # print the card id that get this time
-            # print(card_id)
+# main
+def main():
+    if load_file():
+        connect_and_read_input()
 
-            # if the card id have not vote write into list.txt
-            if card_id not in stu_id_list:
-                stu_id_list.append(card_id)
-                with open('./list.txt', 'a') as stu_id_list_file:
-                    stu_id_list_file.write('{0}\n'.format(card_id))
 
-                print('{0} have not vote before'.format(card_id))
-            else:
-                print('{0} have already vote before'.format(card_id))
-
-            # clear the record
-            card_id = ''
-            cmd = config.CMD_CLEAR_LAST
-            connection.send(bytes(cmd))
+if __name__ == '__main__':
+    main()
